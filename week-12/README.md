15e903ebd33c9809c85a133bd92348fb4f0500dc
zakyuf <mochammadzakyz@gmail.com>
2023-11-18 17:18:26 +0700

    W12: Soal 2

---
+++ week-12/README.md
@@ -0,0 +1,11 @@
+# Praktikum 1: Mengunduh Data dari Web Service (API)
+
+## Soal 1
+Tambahkan <b>nama panggilan Anda</b> pada `title app` sebagai identitas hasil pekerjaan Anda.</br>
+![img](docs/images/soal1.png)
+
+## Soal 2
+Carilah judul buku favorit Anda di Google Books, lalu ganti ID buku pada variabel path di kode tersebut. Caranya ambil di URL browser Anda seperti gambar berikut ini.</br>
+![img](docs/images/soal2_1.png)</br>
+Kemudian cobalah akses di browser URI tersebut dengan lengkap seperti ini. Jika menampilkan data JSON, maka Anda telah berhasil. Lakukan capture milik Anda dan tulis di README pada laporan praktikum. Lalu lakukan commit dengan pesan "<b>W12: Soal 2</b>".</br>
+![img](docs/images/soal2_2.png)
---
+++ week-12/src/books/lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future Demo Zaky',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from the Future'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            ElevatedButton(
+              child: const Text('GO!'),
+              onPressed: () {},
+            ),
+            const Spacer(),
+            Text(result),
+            const Spacer(),
+            const CircularProgressIndicator(),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '/books/v1/volumes/KFK7EAAAQBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+}
